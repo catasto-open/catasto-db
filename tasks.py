7784deb5a2e88548a5e02a6e34e38d35e80dbedd
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2022-06-02 18:58:00 +0200

    Move sql scripts under a sql folder and ignore L016 linting errors

--- tasks.py
+++ tasks.py
@@ -3,6 +3,6 @@ from invoke import task
 
 @task
 def lint(ctx):
-    ctx.run("sqlfluff lint . --dialect postgres")
+    ctx.run("sqlfluff lint ./sql --config ./sql/.sqlfluff")
     ctx.run("flake8")
     ctx.run("black --check .")
