af0ca1122a1c061b991ac4d455a814ca0dc93896
Aontnio Cereicllo <1323093+PascalLike@users.noreply.github.com>
2022-11-28 12:16:04 +0100

    Update cucodcau.sql

--- sql/ctcn/tables/cucodcau.sql
+++ sql/ctcn/tables/cucodcau.sql
@@ -5,7 +5,7 @@
 -- DROP TABLE ctcn.cucodcau;
 
 CREATE TABLE ctcn.cucodcau (
-    cod_causa varchar(1) NOT NULL,
+    cod_causa varchar(3) NOT NULL,
     descrizion varchar(65) NOT NULL,
     CONSTRAINT ctcodcau_pkey PRIMARY KEY (cod_causa)
 );
